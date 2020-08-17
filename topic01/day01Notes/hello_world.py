3a34bd908bbc51b00a65e52ebe6a16cce1a2040b
erdosn <rafael.carrasco@flatironschool.com>
2020-08-17 15:06:04 -0500

    pushing first notebook

---
+++ topic01/day01Notes/hello_world.py
@@ -0,0 +1,26 @@
+# this is a comment
+print("this is a script!!!")
+
+x = 4
+print(x)
+
+
+# let's add something to x
+x += 3 # x = 7
+
+x *= 2 # x = 14
+
+# x /= 3 # x = 4.666666666667
+x //=3 # x = 4 because 4.6666666 rounds down to 4
+
+print(x)
+
+if x > 5:
+    print("x is greater than 5")
+else:
+    print("x is not greater than 5")
+    
+
+a = 12
+
+print(a)
---
+++ topic01/day01Notes/this.is.a.file.py
@@ -0,0 +1,1 @@
+print('this filename is stupid, but it works')
