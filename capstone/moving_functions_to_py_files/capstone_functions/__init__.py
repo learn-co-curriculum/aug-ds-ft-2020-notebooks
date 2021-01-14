50122268b7bde58d703c1fec9e5587d8a6d16fb3
jirvingphd <james.irving.phd@gmail.com>
2021-01-14 15:13:49 -0500

    updated post-SG

--- capstone/moving_functions_to_py_files/capstone_functions/__init__.py
+++ capstone/moving_functions_to_py_files/capstone_functions/__init__.py
@@ -2,5 +2,8 @@
 - James M. Irving, Ph.D.
 - james.irving.phd@gmail.com"""
 
-from capstone_functions import my_functions
+from capstone_functions import my_functions as mf
 from capstone_functions import lz
+
+# from capstone_functions.my_functions import *
+# from capstone_functions.lz import *
