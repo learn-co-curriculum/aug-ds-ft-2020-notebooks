c24813400709e8050067153bb16a85067c503943
jirvingphd <james.irving.phd@gmail.com>
2021-01-23 12:07:35 -0500

    Updated example docstrings

--- capstone/example-docstrings-from-james.py
+++ capstone/example-docstrings-from-james.py
@@ -15,40 +15,15 @@ Example:
 Section breaks are created by resuming unindented text. Section breaks
 are also implicitly created anytime a new section starts.
 
-Attributes:
-    module_level_variable1 (int): Module level variables may be documented in
-        either the ``Attributes`` section of the module docstring, or in an
-        inline docstring immediately following the variable.
-
-        Either form is acceptable, but the two should not be mixed. Choose
-        one convention to document module level variables and be consistent
-        with it.
-
-Todo:
-    * For module TODOs
-    * You have to also use ``sphinx.ext.todo`` extension
-
 .. _Google Python Style Guide:
    http://google.github.io/styleguide/pyguide.html
 
 """
 
-module_level_variable1 = 12345
-
-module_level_variable2 = 98765
-"""int: Module level variable documented inline.
-
-The docstring may span multiple lines. The type may optionally be specified
-on the first line, separated by a colon.
-"""
-
-
+#### JAMES'S RECOMMENDED STYLE/TEMPLATE 
+# Examples are optional, but highly recommended.
 def function_with_types_in_docstring(param1, param2):
     """Example function with types documented in the docstring.
-
-    `PEP 484`_ type annotations are supported. If attribute, parameter, and
-    return types are annotated according to `PEP 484`_, they do not need to be
-    included in the docstring:
 
     Args:
         param1 (int): The first parameter.
@@ -57,12 +32,19 @@ def function_with_types_in_docstring(param1, param2):
     Returns:
         bool: The return value. True for success, False otherwise.
 
-    .. _PEP 484:
-        https://www.python.org/dev/peps/pep-0484/
+    Examples:
+        Examples should be written in doctest format, and should illustrate how
+        to use the function.
 
+        >>> print([i for i in example_generator(4)])
+        [0, 1, 2, 3]
     """
 
 
+
+
+
+############## OTHER MORE ADVANCED TEMPLATES BELOW ################
 def function_with_pep484_type_annotations(param1: int, param2: str) -> bool:
     """Example function with PEP 484 type annotations.
 
@@ -74,108 +56,6 @@ def function_with_pep484_type_annotations(param1: int, param2: str) -> bool:
         The return value. True for success, False otherwise.
 
     """
-
-
-def module_level_function(param1, param2=None, *args, **kwargs):
-    """This is an example of a module level function.
-
-    Function parameters should be documented in the ``Args`` section. The name
-    of each parameter is required. The type and description of each parameter
-    is optional, but should be included if not obvious.
-
-    If \*args or \*\*kwargs are accepted,
-    they should be listed as ``*args`` and ``**kwargs``.
-
-    The format for a parameter is::
-
-        name (type): description
-            The description may span multiple lines. Following
-            lines should be indented. The "(type)" is optional.
-
-            Multiple paragraphs are supported in parameter
-            descriptions.
-
-    Args:
-        param1 (int): The first parameter.
-        param2 (:obj:`str`, optional): The second parameter. Defaults to None.
-            Second line of description should be indented.
-        *args: Variable length argument list.
-        **kwargs: Arbitrary keyword arguments.
-
-    Returns:
-        bool: True if successful, False otherwise.
-
-        The return type is optional and may be specified at the beginning of
-        the ``Returns`` section followed by a colon.
-
-        The ``Returns`` section may span multiple lines and paragraphs.
-        Following lines should be indented to match the first line.
-
-        The ``Returns`` section supports any reStructuredText formatting,
-        including literal blocks::
-
-            {
-                'param1': param1,
-                'param2': param2
-            }
-
-    Raises:
-        AttributeError: The ``Raises`` section is a list of all exceptions
-            that are relevant to the interface.
-        ValueError: If `param2` is equal to `param1`.
-
-    """
-    if param1 == param2:
-        raise ValueError('param1 may not be equal to param2')
-    return True
-
-
-def example_generator(n):
-    """Generators have a ``Yields`` section instead of a ``Returns`` section.
-
-    Args:
-        n (int): The upper limit of the range to generate, from 0 to `n` - 1.
-
-    Yields:
-        int: The next number in the range of 0 to `n` - 1.
-
-    Examples:
-        Examples should be written in doctest format, and should illustrate how
-        to use the function.
-
-        >>> print([i for i in example_generator(4)])
-        [0, 1, 2, 3]
-
-    """
-    for i in range(n):
-        yield i
-
-
-class ExampleError(Exception):
-    """Exceptions are documented in the same way as classes.
-
-    The __init__ method may be documented in either the class level
-    docstring, or as a docstring on the __init__ method itself.
-
-    Either form is acceptable, but the two should not be mixed. Choose one
-    convention to document the __init__ method and be consistent with it.
-
-    Note:
-        Do not include the `self` parameter in the ``Args`` section.
-
-    Args:
-        msg (str): Human readable string describing the exception.
-        code (:obj:`int`, optional): Error code.
-
-    Attributes:
-        msg (str): Human readable string describing the exception.
-        code (int): Exception error code.
-
-    """
-
-    def __init__(self, msg, code):
-        self.msg = msg
-        self.code = code
 
 
 class ExampleClass(object):
